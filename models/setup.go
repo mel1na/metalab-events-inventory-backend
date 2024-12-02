3516b206235a7a4727b71c94cf1e66ce7239ce08
Melina <142022253+mel1na@users.noreply.github.com>
2024-12-02 21:48:03 +0000

    update db url for docker

--- models/setup.go
+++ models/setup.go
@@ -8,7 +8,7 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	dsn := "host=localhost user=test password=test dbname=test port=5432 sslmode=disable timezone=Europe/Vienna"
+	dsn := "host=events-postgres user=test password=test dbname=test port=5432 sslmode=disable timezone=Europe/Vienna"
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{}) // change the database provider if necessary
 
 	if err != nil {
