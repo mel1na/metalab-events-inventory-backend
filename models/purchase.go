5c0ef4c5036321710e190e1a43ab51d4f9a0175b
mel1na <142022253+mel1na@users.noreply.github.com>
2025-01-02 20:21:44 +0100

    try id as foreign key

--- models/purchase.go
+++ models/purchase.go
@@ -4,7 +4,7 @@ import "time"
 
 type Purchase struct {
 	ID          uint      `json:"id"`
-	Items       []Item    `json:"items" gorm:"foreignKey:ParentID;type:bytes;serializer:gob"`
+	Items       []Item    `json:"items" gorm:"foreignKey:ID;type:bytes;serializer:gob"`
 	PaymentType string    `json:"payment_type"`
 	Tip         float32   `json:"tip,omitempty"`
 	FinalCost   float32   `json:"final_cost"`
