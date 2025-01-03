37ea06abc0060c44b58687cb84ff34ef65690321
mel1na <142022253+mel1na@users.noreply.github.com>
2025-01-03 23:31:43 +0100

    add user model

---
+++ models/user.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	UserId    uuid.UUID      `json:"id" gorm:"primaryKey;unique;type:uuid;default:gen_random_uuid()"`
+	Name      string         `json:"name"`
+	Token     jwt.Token      `json:"jwt"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+}
