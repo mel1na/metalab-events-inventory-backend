fd3fb5b90ae4ebc293fabf9a876eb04d78ad43e0
mel1na <142022253+mel1na@users.noreply.github.com>
2025-01-04 01:59:04 +0100

    jwt auth prep

--- models/setup.go
+++ models/setup.go
@@ -1,6 +1,10 @@
 package models
 
 import (
+	"fmt"
+	"os"
+
+	"github.com/golang-jwt/jwt/v5"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -17,6 +21,25 @@ func ConnectDatabase() {
 
 	database.AutoMigrate(&Item{})
 	database.AutoMigrate(&Purchase{})
+	database.AutoMigrate(&User{})
+
+	if database.Limit(1).Find(&User{Name: "admin"}).RowsAffected == 0 {
+
+		key := []byte(os.Getenv("JWT_SECRET"))
+		t := jwt.NewWithClaims(jwt.SigningMethodHS512, jwt.MapClaims{
+			"iss":   "metalab-events-backend",
+			"sub":   "admin",
+			"admin": true,
+		})
+		s, err := t.SignedString(key)
+		fmt.Println(s)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			database.Create(&User{Name: "admin", Token: s})
+			fmt.Printf("Default admin user created with token %s\n", s)
+		}
+	}
 
 	DB = database
 }
--- models/user.go
+++ models/user.go
@@ -3,7 +3,6 @@ package models
 import (
 	"time"
 
-	"github.com/golang-jwt/jwt/v5"
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
@@ -11,7 +10,7 @@ import (
 type User struct {
 	UserID    uuid.UUID      `json:"id" gorm:"primaryKey;unique;type:uuid;default:gen_random_uuid()"`
 	Name      string         `json:"name"`
-	Token     jwt.Token      `json:"jwt"`
+	Token     string         `json:"jwt"`
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
 	DeletedAt gorm.DeletedAt `gorm:"index"`
