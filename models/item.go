8bb0db0bb810cd1cd448f9aaf510751e41106487
mel1na <142022253+mel1na@users.noreply.github.com>
2025-01-19 23:01:12 +0100

    remove parentid

--- models/item.go
+++ models/item.go
@@ -4,7 +4,6 @@ import "time"
 
 type Item struct {
 	ItemID    uint      `json:"id" gorm:"primaryKey;unique"`
-	ParentID  uint      `json:"parent_id,omitempty"`
 	Name      string    `json:"name"`
 	Quantity  uint      `json:"quantity,omitempty"`
 	Price     float32   `json:"price"`
