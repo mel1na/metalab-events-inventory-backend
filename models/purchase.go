ee1e3465d2b5db845f733d5c972550bd11cf9cc7
mel1na <142022253+mel1na@users.noreply.github.com>
2025-01-21 16:44:14 +0100

    prep new types for payment model

--- models/purchase.go
+++ models/purchase.go
@@ -15,3 +15,33 @@ type Purchase struct {
 	FinalCost     uint      `json:"final_cost"`
 	CreatedAt     time.Time `json:"created_at"`
 }
+
+// PaymentStatus: The status of the payment object gives information about the current state of the payment.
+//
+// Possible values:
+//
+// - `unknown` - The payment status is unknown.
+// - `unpaid` - The payment has not been paid for (yet).
+// - `paid` - The payment has been processed and paid.
+type PaymentStatus string
+
+const (
+	PaymentStatusUnknown PaymentStatus = "unknown"
+	PaymentStatusUnpaid  PaymentStatus = "unpaid"
+	PaymentStatusPaid    PaymentStatus = "paid"
+)
+
+// PaymentType: The type of the payment object gives information about the type of payment.
+//
+// Possible values:
+//
+// - `cash` - The payment was made with cash.
+// - `unpaid` - The payment was made with a credit/debit card.
+// - `paid` - The reader is paired with a merchant account and can be used with SumUp APIs.
+type PaymentType string
+
+const (
+	PaymentTypeCash  PaymentType = "cash"
+	PaymentTypeCard  PaymentType = "card"
+	PaymentTypeOther PaymentType = "other"
+)
