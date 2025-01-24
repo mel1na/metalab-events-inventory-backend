dbe62e1fc7c7596bf648fdc1d89767fffd196d9a
mel1na <142022253+mel1na@users.noreply.github.com>
2025-01-24 20:38:15 +0100

    improve logging

--- sumup_integration/sumup.go
+++ sumup_integration/sumup.go
@@ -16,11 +16,11 @@ func Login() {
 
 	account, err := SumupClient.Merchant.Get(context.Background(), sumup.GetAccountParams{})
 	if err != nil {
-		fmt.Printf("SumUp API: get merchant account: %s\n", err.Error())
+		fmt.Printf("[ERROR] SumUp API: error getting merchant account: %s\n", err.Error())
 		return
 	}
 
-	fmt.Printf("SumUp API: authorized for merchant %q\n\n", *account.MerchantProfile.MerchantCode)
+	fmt.Printf("SumUp API: authorized for merchant %q (%s)\n\n", *account.MerchantProfile.MerchantCode, *account.MerchantProfile.CompanyName)
 	SumupAccount = account
 }
 
@@ -28,7 +28,7 @@ func StartReaderCheckout(ReaderId string, TotalAmount uint) (ClientTransactionId
 	var returnUrl string = os.Getenv("SUMUP_RETURN_URL")
 	response, checkout_err := SumupClient.Readers.CreateCheckout(context.Background(), *SumupAccount.MerchantProfile.MerchantCode, ReaderId, sumup.CreateReaderCheckoutBody{ReturnUrl: &returnUrl, TotalAmount: sumup.CreateReaderCheckoutAmount{Currency: "EUR", MinorUnit: 2, Value: int(TotalAmount)}})
 	if checkout_err != nil {
-		return "error", fmt.Errorf("error while creating reader checkout: %s\n", checkout_err.Error())
+		return "error", fmt.Errorf("[ERROR] SumUp API: error while creating reader checkout: %s\n", checkout_err.Error())
 	}
 	return *response.Data.ClientTransactionId, nil
 }
